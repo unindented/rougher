b3108a1e9628cbf6ae01cd9862a214ea95cd532e
Chris Wilkinson <c.wilkinson@elifesciences.org>
2021-07-28 19:55:48 +0100

    fix: serialize SVGs correctly
    Serializes SVGs as a complete XML document, meaning that nodes aren't dropped.

--- src/node/index.ts
+++ src/node/index.ts
@@ -1,15 +1,15 @@
 import { JSDOM } from "jsdom";
 import { Options } from "roughjs/bin/core";
+import serializer from "w3c-xmlserializer";
 
 import roughUp from "../shared";
 
 export default (input: string, options: Options = {}): string => {
-  const { window } = new JSDOM();
+  const { window } = new JSDOM(input, { contentType: "image/svg+xml" });
 
-  window.document.body.innerHTML = input;
   const svg = window.document.querySelector("svg") as SVGSVGElement;
 
   roughUp(svg, options);
 
-  return svg.outerHTML;
+  return serializer(window.document);
 };
